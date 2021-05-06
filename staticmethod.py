2457addc6f15156a54d3a1b9129158fc62d0a49e
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2021-05-06 23:36:36 +0530

    Create staticmethod.py
    This  helps us to call out the methods with out calling the methods parameter

---
+++ staticmethod.py
@@ -0,0 +1,8 @@
+class Staticmethod():
+
+    @staticmethod                             #important key here it helps us to call out the methods with out calling the methods parameter
+    def static():
+        print("This method doesn't require the argument in method")
+
+jd=Staticmethod()
+jd.static()
